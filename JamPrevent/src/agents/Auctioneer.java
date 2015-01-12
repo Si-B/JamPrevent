fce549ca7f457c177b667bac570b15fc7f4052c3
Si-B <si.b@gmx.de>
2015-01-12 16:35:39 +0100

    First sending of data

--- JamPrevent/src/agents/Auctioneer.java
+++ JamPrevent/src/agents/Auctioneer.java
@@ -7,12 +7,18 @@ package agents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.WakerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -20,24 +26,67 @@ import java.util.List;
  * @author SiB
  */
 public class Auctioneer extends Agent {
-    
+
     private final List<AID> trafficLightAgents = new ArrayList<>();
+    private HashMap<AID, HashMap<String, String>> trafficLightsMetadata = new HashMap<>();
 
     @Override
-    public void setup() {                
+    public void setup() {
         addBehaviour(new WakerBehaviour(this, 1000) {
-
             @Override
             protected void onWake() {
                 super.onWake(); //To change body of generated methods, choose Tools | Templates.
                 findAndAddTrafficLights();
                 trafficLightAgents.stream().forEach((agent) -> {
+                    trafficLightsMetadata.put(agent, new HashMap<String, String>());
                     System.out.println("Found Trafficlight: " + agent.getLocalName());
+                    AskTrafficLightForPosition(agent);
+                    //System.out.println("which is: " + agent.get());
+
                 });
-            }            
-        });                
-    }
+            }
+        });
         
+        addBehaviour(new WakerBehaviour(this, 2500) {
+
+            @Override
+            protected void onWake() {
+                super.onWake(); //To change body of generated methods, choose Tools | Templates.
+                
+            }                            
+        });
+
+        //receiving
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+
+                ACLMessage msg = receive(mt);
+                if (msg != null) {
+                    System.out.println(msg.getSender().getLocalName() + " sent to " + myAgent.getLocalName() + " -> " + msg.getContent());
+
+                    if (msg.getContent().equalsIgnoreCase("getLocation")) {
+                        if (msg.getAllUserDefinedParameters().size() > 0) {
+                            if (trafficLightsMetadata.containsKey(msg.getSender())) {
+                                trafficLightsMetadata.get(msg.getSender()).put("location", msg.getAllUserDefinedParameters().getProperty("location"));
+                            }
+                            System.out.println(msg.getAllUserDefinedParameters().getProperty("location"));
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void AskTrafficLightForPosition(AID trafficLight) {
+        jade.lang.acl.ACLMessage message = new jade.lang.acl.ACLMessage(
+                jade.lang.acl.ACLMessage.REQUEST);
+        message.addReceiver(trafficLight);
+        message.setContent("getLocation");
+        this.send(message);
+    }
+
     private void findAndAddTrafficLights() {
         DFAgentDescription template = new DFAgentDescription();
         ServiceDescription sd = new ServiceDescription();
@@ -48,11 +97,11 @@ public class Auctioneer extends Agent {
             DFAgentDescription[] dfds = DFService.search(this, template);
 
             if (dfds.length > 0) {
-                for(DFAgentDescription trafficLightAgentDescription :  dfds){
+                for (DFAgentDescription trafficLightAgentDescription : dfds) {
                     AID trafficLightAgent = trafficLightAgentDescription.getName();
                     trafficLightAgents.add(trafficLightAgent);
-                }                                                
-            }                      
+                }
+            }
         } catch (FIPAException fe) {
             fe.printStackTrace();
         }
--- JamPrevent/src/agents/TrafficLight.java
+++ JamPrevent/src/agents/TrafficLight.java
@@ -6,21 +6,59 @@
 package agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 /**
  *
  * @author SiB
  */
 public class TrafficLight extends Agent{
+    private String location;
 
     @Override
     public void setup() {
+        
+        Object[] arguments = getArguments();
+        
+        if(arguments.length > 0){
+            location = arguments[0].toString();
+        }
+        addBehaviour(new TellLocationBehaviour());
         registerAgent();
     }
+    
+    private class TellLocationBehaviour extends CyclicBehaviour {
+		private static final long serialVersionUID = -5018397038252984135L;
+
+		public void action() {
+			MessageTemplate tmp = MessageTemplate
+					.MatchPerformative(ACLMessage.REQUEST);
+			ACLMessage m = receive(tmp);
+
+			if (m != null) {
+				ACLMessage reply = m.createReply();
+				String message = m.getContent();
+
+				if (message.equals("getLocation")) {
+                                    System.out.println("SENDING LOCATION");
+                                    reply.setContent("getLocation");
+                                    reply.setPerformative(ACLMessage.INFORM);
+                                    reply.addUserDefinedParameter("location", getLocation());                                    
+				} else {
+					System.out.println("not understood");
+					reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+					// Reply with not-understood
+				}
+				this.myAgent.send(reply);
+			}
+		}
+	}
         
     private void registerAgent() {
         DFAgentDescription dfd = new DFAgentDescription();
@@ -37,6 +75,13 @@ public class TrafficLight extends Agent{
         } catch (FIPAException fe) {
                 fe.printStackTrace();
         }
+    }
+
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
     }
     
 }
