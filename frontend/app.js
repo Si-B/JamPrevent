1e4122fa55b4ea52d42214310ba5b9791c7a6642
Knut Hühne <knut@k-nut.eu>
2015-01-30 14:22:23 +0100

    Adapt frontend for one more light

--- frontend/app.js
+++ frontend/app.js
@@ -1,18 +1,25 @@
-$(document).ready(function(){
-  setInterval(updatePage, 100);
-});
+/*eslint-env browser*/
+/*eslint no-use-before-define:0 */
+/*global $*/
 
-
-function updatePage(){
-  $.getJSON('./state.json', updateImage);
-}
-//function (data, textStatus, jqXHR) {
-
-
-
-function updateImage(data){
-  $.each(data, function(index, value){
-    document.getElementById(value.location).style.fill = value.state;
-    document.getElementById(value.location + "-load").innerHTML = value.load;
+(function(){
+  "use strict";
+  $(document).ready(function(){
+    setInterval(updatePage, 100);
   });
-}
+
+
+  function updatePage(){
+    $.getJSON("./state.json", updateImage);
+  }
+  //function (data, textStatus, jqXHR) {
+
+
+
+  function updateImage(data){
+    $.each(data, function(index, value){
+      document.getElementById(value.location + "2" + value.direction).style.fill = value.state;
+      document.getElementById(value.location + "2" + value.direction + "-load").innerHTML = value.load;
+    });
+  }
+})();
