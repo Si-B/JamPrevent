855b1f8496fca7009dcc3934de1d02450ea35440
Knut Hühne <knut@k-nut.eu>
2015-02-02 17:19:18 +0100

    Display all lights

--- frontend/app.js
+++ frontend/app.js
@@ -12,14 +12,32 @@
   function updatePage(){
     $.getJSON("./state.json", updateImage);
   }
-  //function (data, textStatus, jqXHR) {
 
-
-
-  function updateImage(data){
+  function updateLights(data){
     $.each(data, function(index, value){
       document.getElementById(value.location + "2" + value.direction).style.fill = value.state;
       document.getElementById(value.location + "2" + value.direction + "-load").innerHTML = value.load;
     });
   }
+
+  function updateImage(data){
+    updateLights(data);
+    colorHighest(data);
+  }
+
+
+  function colorHighest(data){
+    var carCounts = $.map(data, function(d){return d.load});
+    var highestCarCount = Array.max(carCounts);
+    $.each(data, function(index, value){
+      var color = value.load === highestCarCount ? "yellow": "black";
+      document.getElementById(value.location + "2" + value.direction + "-load").style.fill = color;
+    });
+  }
+
+
+  Array.max = function( array ){
+    return Math.max.apply( Math, array );
+  };
 })();
+
