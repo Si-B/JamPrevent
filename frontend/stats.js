4d55c9b14eaafe3b399c6382b17a2a4b8d9b7818
Knut Hühne <knut@k-nut.eu>
2015-02-09 11:55:07 +0100

    Autoupdate values

--- frontend/stats.js
+++ frontend/stats.js
@@ -1,19 +1,21 @@
 (function () {
-	$.getJSON("history.json", function(data){
-		console.log(data);
-		var grouped = _.groupBy(data, function(d){return d.index;});
-		var sums = _.mapValues(grouped, function(values){
-			return _.reduce(values, function(total, current){
-				return total + current.load;
-			}, 0);
-		});
-		var data = _.map(sums, function(value, key){
-			return [parseInt(key), value];
-		})
-		console.log(grouped);
-		console.log(sums);
-		console.log(data);
+	window.setInterval(function(){
+		$.getJSON("history.json", function(data){
+			console.log(data);
+			var grouped = _.groupBy(data, function(d){return d.index;});
+			var sums = _.mapValues(grouped, function(values){
+				return _.reduce(values, function(total, current){
+					return total + current.load;
+				}, 0);
+			});
+			var data = _.map(sums, function(value, key){
+				return [parseInt(key), value];
+			})
+			console.log(grouped);
+			console.log(sums);
+			console.log(data);
 
-		$.plot("#plot", [data]);
-	});
+			$.plot("#plot", [data]);
+		});
+	}, 100);
 })();
