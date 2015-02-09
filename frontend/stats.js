b6ec51d6da44fcad6404762e1906ce12af7155b0
Knut Hühne <knut@k-nut.eu>
2015-02-09 14:17:35 +0100

    Make individual statistics

--- frontend/stats.js
+++ frontend/stats.js
@@ -4,7 +4,7 @@
 		$.getJSON("history.json", function(data){
 			var grouped = _.groupBy(data, function(d){return d.index;});
 			plotDifferences(grouped);
-			plotSums(grouped);
+			plotSums(data);
 		});
 	}, 100);
 
@@ -27,16 +27,45 @@
 		$.plot("#differences", [differences]);
 	}
 
-	function plotSums(grouped){
-		var sums = _.mapValues(grouped, function(values){
+
+	function sumLoads(data){
+		return _.mapValues(data, function(values){
 			return _.reduce(values, function(total, current){
 				return total + current.load;
 			}, 0);
 		});
 
-		var data = createArrayFromDict(sums);
+	}
 
-		$.plot("#plot", [data]);
+	function plotSums(data){
+		var sh = _.filter(data, function(d){return d.crossLocation === "SingleHeighest";});
+		var ra = _.filter(data, function(d){return d.crossLocation === "Random";});
+		var pr = _.filter(data, function(d){return d.crossLocation === "Predefined";});
+
+		sh = _.groupBy(sh, function(d){return d.index;});
+		ra = _.groupBy(ra, function(d){return d.index;});
+		pr = _.groupBy(pr, function(d){return d.index;});
+
+
+		var SingleHeighestSums = sumLoads(sh);
+		var RandomSums = sumLoads(ra);
+		var PredefinedSums = sumLoads(pr);
+
+		var randomData = createArrayFromDict(RandomSums);
+		var shData = createArrayFromDict(SingleHeighestSums);
+		var prData = createArrayFromDict(PredefinedSums);
+
+		$.plot("#plot", [
+				{"label": "Zufällig", 
+				 "data":  randomData
+				},
+				{"label": "Single Highest",
+				 "data": shData
+				},
+				{"label": "Predefined",
+				 "data": prData
+				}
+				]);
 	}
 
 })();
