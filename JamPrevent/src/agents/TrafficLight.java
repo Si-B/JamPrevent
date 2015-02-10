aecd6d09b019a0e6a05415882583681c9dae1f02
Si-B <si.b@gmx.de>
2015-02-10 14:10:44 +0100

    Runconfig

--- JamPrevent/src/agents/TrafficLight.java
+++ JamPrevent/src/agents/TrafficLight.java
@@ -28,6 +28,7 @@ import messages.TrafficLightState;
 /**
  *
  * @author SiB
+ * -gui -agents "WestSouthLight:agents.TrafficLight(W,S,Random);WestEastLight:agents.TrafficLight(W,E,Random);EastWestLight:agents.TrafficLight(E,W,Random);EastSouthLight:agents.TrafficLight(E,S,Random);SouthEastLight:agents.TrafficLight(S,E,Random);SouthWestLight:agents.TrafficLight(S,W,Random);Auctioneer:agents.Auctioneer(Random);WestSouthLightSingleHeighest:agents.TrafficLight(W,S,SingleHeighest);WestEastLightSingleHeighest:agents.TrafficLight(W,E,SingleHeighest);EastWestLightSingleHeighest:agents.TrafficLight(E,W,SingleHeighest);EastSouthLightSingleHeighest:agents.TrafficLight(E,S,SingleHeighest);SouthEastLightSingleHeighest:agents.TrafficLight(S,E,SingleHeighest);SouthWestLightSingleHeighest:agents.TrafficLight(S,W,SingleHeighest);AuctioneerSingleHeighest:agents.Auctioneer(SingleHeighest);WestSouthLightPredefined:agents.TrafficLight(W,S,Predefined);WestEastLightPredefined:agents.TrafficLight(W,E,Predefined);EastWestLightPredefined:agents.TrafficLight(E,W,Predefined);EastSouthLightPredefined:agents.TrafficLight(E,S,Predefined);SouthEastLightPredefined:agents.TrafficLight(S,E,Predefined);SouthWestLightPredefined:agents.TrafficLight(S,W,Predefined);AuctioneerPredefined:agents.Auctioneer(Predefined);ReportProvider:agents.ReportingAgent(C:\Users\SiB\Documents\GitHub\JamPrevent\frontend\);Simulator:agents.LoadSimulatorAgent"
  */
 public class TrafficLight extends BaseAgent{
 
